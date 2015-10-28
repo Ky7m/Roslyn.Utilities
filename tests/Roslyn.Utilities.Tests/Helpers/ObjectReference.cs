237190659700d56fe4695a40e8235889061e986c
Igor Fesenko <igor.aka.ky7m@gmail.com>
2015-10-28 23:22:46 +0200

    Add WeakList and SpecializedCollections empty instance definitions

---
+++ tests/Roslyn.Utilities.Tests/Helpers/ObjectReference.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Roslyn.Utilities.Tests.Helpers
+{
+    internal class ObjectReference
+    {
+        public object Strong;
+        public readonly WeakReference Weak;
+
+        public ObjectReference(object target)
+        {
+            this.Strong = target;
+            this.Weak = new WeakReference(target);
+        }
+
+        public ObjectReference() : this(new object())
+        {
+        }
+    }
+}
