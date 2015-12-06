bb3ef6a97c5b15e8a6f9474e420b03db67fbe9a2
Igor Fesenko <igor.aka.ky7m@gmail.com>
2015-12-06 13:16:29 +0200

    Bump version to 1.0.3

--- src/Roslyn.Utilities/Properties/AssemblyInfo.cs
+++ src/Roslyn.Utilities/Properties/AssemblyInfo.cs
@@ -3,8 +3,8 @@ using System.Runtime.CompilerServices;
 
 [assembly: AssemblyTitle("Roslyn.Utilities")]
 [assembly: AssemblyProduct("Roslyn.Utilities")]
-[assembly: AssemblyVersion("1.0.2")]
-[assembly: AssemblyFileVersion("1.0.2")]
+[assembly: AssemblyVersion("1.0.3")]
+[assembly: AssemblyFileVersion("1.0.3")]
 
 
 [assembly: InternalsVisibleTo("Roslyn.Utilities.Tests")]
